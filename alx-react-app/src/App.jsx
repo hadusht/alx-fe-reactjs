ffa7d121e5cdf3d12c8e6f0048289c34b472c161
Hadush Tekulu <hadetek@gmail.com>
2025-08-31 14:35:30 +0300

    add new alx-react-app update3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import WelcomeMessage from "./components/WelcomeMessage";
-
-
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 
 
@@ -16,7 +17,12 @@ function App() {
   return (
     
     <>
-    <WelcomeMessage />;
+    <WelcomeMessage />
+    <Header />
+    <MainContent />
+    <Footer />
+    
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -5,7 +5,7 @@ function WelcomeMessage() {
             <p>This is a simple JSX component.</p>
             <p>I am learning about JSX!.</p>
         </div>
-    );
+    )
 }
 
 export default WelcomeMessage;
