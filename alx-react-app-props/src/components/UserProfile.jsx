774a1f51dbf445d7451ab5f782619a972863207c
Hadush Tekulu <hadetek@gmail.com>
2025-09-07 14:38:24 +0300

    add new alx-react-app-new file add props1

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,25 +1,16 @@
-// src/components/UserProfile.jsx
+// src/UserProfile.jsx
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
-const UserProfile = (props) => {
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
-    <div style={{
-      border: "2px solid #ccc",
-      borderRadius: "10px",
-      padding: "15px",
-      width: "250px",
-      margin: "10px auto",
-      textAlign: "center",
-      backgroundColor: "blue"
-      
-    }}>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p> {/* Add the "Bio: " label */}
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
-
-
-
   );
-};
+}
 
 export default UserProfile;
