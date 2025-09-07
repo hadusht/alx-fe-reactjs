52c4725c6470d5f27f4ecbb330ff4f452e20a267
Hadush Tekulu <hadetek@gmail.com>
2025-09-07 16:13:18 +0300

    add new my-company3

--- my-company/src/Components/Navbar.jsx
+++ my-company/src/Components/Navbar.jsx
@@ -1,27 +1,14 @@
-import { Link } from "react-router-dom";
+// src/components/Navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav
-      style={{
-        padding: "10px",
-        backgroundColor: "#333",
-        display: "flex",
-        justifyContent: "space-around",
-      }}
-    >
-      <Link to="/" style={{ color: "white", textDecoration: "none" }}>
-        Home
-      </Link>
-      <Link to="/about" style={{ color: "white", textDecoration: "none" }}>
-        About
-      </Link>
-      <Link to="/services" style={{ color: "white", textDecoration: "none" }}>
-        Services
-      </Link>
-      <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>
-        Contact
-      </Link>
+    <nav style={{ backgroundColor: '#333', padding: '10px' }}>
+      <Link to="/" style={{ color: 'white', marginRight: '15px', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: 'white', marginRight: '15px', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ color: 'white', marginRight: '15px', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
     </nav>
   );
 }
