af54128d47dc97954a41953c81a87d579c697d01
Hadush Tekulu <hadetek@gmail.com>
2025-09-15 16:22:38 +0300

    add new receip-sharing file3

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,4 +4,28 @@ const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
+
+  export const useRecipeStore = create((set) => ({
+  recipes: [
+    { id: '1', title: 'Fluffy Pancakes', description: 'Easy breakfast pancakes everyone loves.' },
+    { id: '2', title: 'Spaghetti Pomodoro', description: 'Classic tomato-based spaghetti.' }
+  ],
+
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, { ...recipe, id: Date.now().toString() }]
+    })),
+
+  updateRecipe: (id, updatedFields) =>
+    set((state) => ({
+      recipes: state.recipes.map((r) =>
+        r.id === id ? { ...r, ...updatedFields } : r
+      )
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((r) => r.id !== id)
+    }))
+}));
 }));
