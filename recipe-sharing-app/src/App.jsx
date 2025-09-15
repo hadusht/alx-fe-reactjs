290a051710fad896dfffa249f0d887416112a11b
Hadush Tekulu <hadetek@gmail.com>
2025-09-15 12:27:12 +0300

    add new receip-sharing file

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RecipeList from './components/RecipeList.js'
+import AddRecipeForm from './components/AddRecipeForm.js'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  )
+}
+
+export default App
