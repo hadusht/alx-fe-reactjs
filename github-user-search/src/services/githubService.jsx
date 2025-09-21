5770f304ee48764f2fbcd622257635d11982acf2
Hadush Tekulu <hadetek@gmail.com>
2025-09-21 14:25:45 +0300

    add a github-user-search file to github4

--- github-user-search/src/services/githubService.jsx
+++ github-user-search/src/services/githubService.jsx
@@ -1,11 +1,13 @@
-const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
-const GITHUB_API_URL = import.meta.env.VITE_APP_GITHUB_API_URL;
+import axios from "axios";
 
-export async function fetchRepos(username) {
-  const response = await fetch(`${GITHUB_API_URL}/users/${username}/repos`, {
+const API_URL = import.meta.env.VITE_APP_GITHUB_API_URL;
+const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
+
+export async function fetchUser(username) {
+  const response = await axios.get(`${API_URL}/users/${username}`, {
     headers: {
-      Authorization: `token ${GITHUB_API_KEY}`,
+      Authorization: `token ${API_KEY}`, // optional for unauthenticated requests
     },
   });
-  return response.json();
+  return response.data;
 }
