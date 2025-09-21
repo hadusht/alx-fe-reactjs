a8aa88d947e46d1b3d102e8903cfb07e967f6f9e
Hadush Tekulu <hadetek@gmail.com>
2025-09-21 16:04:11 +0300

    add Integrating the GitHub API and Creating a Basic Search Feature2

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -19,7 +19,7 @@ export default function Search() {
       const data = await fetchUserData(username);
       setUser(data);
     } catch {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
