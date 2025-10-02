ab89b356869f31a7e99749d05ded20627e72f37b
Hadush Tekulu <hadetek@gmail.com>
2025-10-02 22:32:19 +0300

    add 0. Setting Up a React Application for a Recipe Sharing Platform with Tailwind CSS

--- recipe-sharing-platform/vite.config.js
+++ recipe-sharing-platform/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
